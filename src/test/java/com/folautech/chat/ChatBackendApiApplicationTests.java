72f0b2ef6249ee06a81505d1cc8edcb96dbf13da
folaulau <folaukaveinga@gmail.com>
2022-05-21 11:04:23 -0600

    setup

---
+++ src/test/java/com/folautech/chat/ChatBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
